fee4192b8de26217d6d2a127af0e277787af8410
DavidDuveau <david.duveau@hotmail.fr>
2020-07-22 03:21:04 +0200

    sidenav random img added

--- assets/js/mainPage.js
+++ assets/js/mainPage.js
@@ -2,3 +2,37 @@ document.addEventListener("DOMContentLoaded", function () {
   let elems = document.querySelectorAll(".sidenav");
   let instances = M.Sidenav.init(elems);
 });
+
+/* function changeImage() {
+  let images = document.getElementById("img-sidenav_0");
+  let src = images.getAttribute("src");
+  if (src == "/assets/images/mushroom.png") {
+    src = "/assets/images/star.png";
+  } else if (src == "/assets/images/star.png") {
+    src = "/assets/images/coin.png";
+  } else {
+    src = "/assets/images/mushroom.png";
+  }
+  images.setAttribute("src", src);
+} */
+
+// fonction pour changer l'image de la sidenav onclick
+let i = 0;
+function changeImage() {
+  let imgArray = [
+    "/assets/images/mushroom.png",
+    "/assets/images/star.png",
+    "/assets/images/coin.png",
+    "/assets/images/triforce.png",
+    "/assets/images/link.png",
+    "/assets/images/boo.png",
+    "/assets/images/m_bros.png",
+    "/assets/images/mask.png",
+    "/assets/images/yoshi.png",
+  ];
+  let images = document.getElementById("img-sidenav_0");
+  let src = images.getAttribute("src");
+  i = Math.floor(Math.random() * 9);
+  src = imgArray[i];
+  images.setAttribute("src", src);
+}
